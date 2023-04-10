3c36120343337f1e7ee8f0615f3410d753da10dd
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-04-10 15:51:37 -0400

    tidy

--- api.js
+++ api.js
@@ -3,7 +3,6 @@ const BaseUrl = "http://api.artsdata.ca"
 const MintUrl =`${BaseUrl}/mint`;
 const QueryUrl = `${BaseUrl}/query`;
 const LinkUrl = `${BaseUrl}/link`;
-//http://localhost:3003/query?adid=K5-1&format=jsonld&sparql=viewer/viewer&frame=viewer/viewer
 
 export {
   MintUrl,
