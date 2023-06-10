79b81e86356cd8caa6184933c44da46f612583c0
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-06-10 15:09:11 -0400

    added address sparql

--- dereference-name.js
+++ dereference-name.js
@@ -5,7 +5,7 @@ class DereferenceName extends HTMLElement {
     async function cc(e) {
       let url = "https://db.artsdata.ca/repositories/artsdata"
       if (e.innerHTML.startsWith('http') ) {
-        let sparql = `construct { <${e.innerHTML}> <http://schema.org/name> ?c ; a ?type }  where { graph ?g { <${e.innerHTML}> <http://schema.org/name> ?c ; a ?type }} `
+        let sparql = `construct { <${e.innerHTML}> <http://schema.org/name> ?c ; a ?type ;  <http://schema.org/streetAddress> ?street  ;  <http://schema.org/addressLocality> ?locality ;  <http://schema.org/postalCode> ?postalCode . }  where { graph ?g {  <${e.innerHTML}> a ?type  OPTIONAL { <${e.innerHTML}> <http://schema.org/name> ?c . } OPTIONAL { <${e.innerHTML}>   <http://schema.org/streetAddress> ?street ; <http://schema.org/addressLocality> ?locality ;  <http://schema.org/postalCode> ?postalCode . }  }} `
        
           const res = await fetch(url, {
             method: "POST",
@@ -18,7 +18,10 @@ class DereferenceName extends HTMLElement {
           const json = await res.json();
           console.log(e.innerHTML,json)
           if (json.length > 0) {
-              e.innerHTML = `<div class="statement-object"><b>${json[0]["http://schema.org/name"][0]["@value"]}</b> <div class="alternate-name">${json[0]["@type"]}</div><rdf-link>${e.innerHTML}</rdf-link></div>`
+              e.innerHTML = `<div class="statement-object">
+                <b>${json[0]?.["http://schema.org/name"]?.[0]?.["@value"] ||  json[0]["http://schema.org/streetAddress"][0]["@value"] + ', ' + json[0]["http://schema.org/addressLocality"][0]["@value"] + ', ' + json[0]["http://schema.org/postalCode"][0]["@value"] }</b> 
+                <div class="alternate-name">${json[0]["@type"] }</div>
+                <rdf-link>${e.innerHTML}</rdf-link></div>`
           } else {
             e.innerHTML = `<rdf-link>${e.innerHTML}</rdf-link>`
           }
